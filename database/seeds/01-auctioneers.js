f94cb7fd5a8f689a2729279252982d693a27c9b1
josegocampo <jose.ocampo@mail.udp.cl>
2020-05-28 21:48:07 -0400

    finished fixing the seeds

--- database/seeds/01-auctioneers.js
+++ database/seeds/01-auctioneers.js
@@ -1,8 +1,10 @@
+const bcrypt = require("bcryptjs")
+
 exports.seed = async function(knex) {
 	await knex("auctioneers").truncate()
 	await knex("auctioneers").insert([
-    {id: 1, name: 'jose', password: '12345'},
-    {id: 2, name: 'cobrettie', password: '67890'},
-    {id: 3, name: 'kaleb', password: '09876'}
+    {id: 1, name: 'jose', password: await bcrypt.hash("12345", 14)},
+    {id: 2, name: 'cobrettie', password: await bcrypt.hash("67890", 14)},
+    {id: 3, name: 'kaleb', password: await bcrypt.hash("09876", 14)}
 	])
 }
